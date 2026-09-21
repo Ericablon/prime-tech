bf2e2612f9670a6277f389e694448561ec42c1c2
Eric ablon <152393745+Ericablon@users.noreply.github.com>
2026-09-21 13:08:29 -0300

    Refactor main.tsx to include PrimeTechProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,39 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import { BrowserRouter } from 'react-router-dom';import App from './App';import { AuthProvider } from './contexts/AuthContext';import './styles.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><AuthProvider><App/></AuthProvider></BrowserRouter></React.StrictMode>)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-if ('serviceWorker' in navigator && import.meta.env.PROD) window.addEventListener('load',()=>void navigator.serviceWorker.register('/sw.js'));
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+
+import { AuthProvider } from './contexts/AuthContext';
+import { PrimeTechProvider } from './contexts/PrimeTechContext';
+
+import './styles.css';
+
+ReactDOM.createRoot(
+  document.getElementById('root')!,
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <PrimeTechProvider>
+          <App />
+        </PrimeTechProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
+if (
+  'serviceWorker' in navigator &&
+  import.meta.env.PROD
+) {
+  window.addEventListener(
+    'load',
+    () => {
+      void navigator.serviceWorker.register(
+        `${import.meta.env.BASE_URL}sw.js`,
+      );
+    },
+  );
+}
