74040a2ea777a849b173dc566f079e7b0fbf790c
Eric ablon <152393745+Ericablon@users.noreply.github.com>
2026-09-22 14:24:22 -0300

    feat: ativar tema claro e escuro no Cronos

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import App from './App';
 
 import { AuthProvider } from './contexts/AuthContext';
 import { PrimeTechProvider } from './contexts/PrimeTechContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 import './styles.css';
 import './premium.css';
 import './login-v2.css';
 import './technician-mobile.css';
+import './theme.css';
 
 const routerBase = import.meta.env.BASE_URL.replace(/\/$/, '') || '/';
 
@@ -27,13 +29,15 @@ try {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBase}>
-      <AuthProvider>
-        <PrimeTechProvider>
-          <App />
-        </PrimeTechProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter basename={routerBase}>
+        <AuthProvider>
+          <PrimeTechProvider>
+            <App />
+          </PrimeTechProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
